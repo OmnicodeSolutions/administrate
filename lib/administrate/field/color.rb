e7dce45929dcb09e893488be3254756b929c4ad7
Mauricio Vieira <mauricio@mauriciovieira.net>
2019-09-11 22:41:33 -0300

    Load Field::Color and add to_s

--- lib/administrate/field/color.rb
+++ lib/administrate/field/color.rb
@@ -2,9 +2,17 @@ require_relative "base"
 
 module Administrate
   module Field
-    class Color < Base
+    class Color < Field::Base
       def self.searchable?
         true
+      end
+
+      def to_s
+        if data.nil?
+          "-"
+        else
+          data.to_s
+        end
       end
     end
   end
