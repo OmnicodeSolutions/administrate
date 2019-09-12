ee552f348c3d2391b5828d4d666a6baeabd12fe3
Mauricio Vieira <mauricio@mauriciovieira.net>
2019-09-11 21:33:29 -0300

    Add color field
    https://github.com/danbee/administrate-field-color is not compatible, since it requires 0.2.0 version and currently it's on 0.12.0

---
+++ lib/administrate/field/color.rb
@@ -0,0 +1,8 @@
+require_relative "base"
+
+module Administrate
+  module Field
+    class Color < Base
+    end
+  end
+end
---
+++ spec/lib/fields/color_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+require "administrate/field/color"
+
+describe Administrate::Field::Color do
+  describe "#to_partial_path" do
+    it "returns a partial based on the page being rendered" do
+      page = :show
+      field = Administrate::Field::Color.new(:color, "#ea4049", page)
+
+      path = field.to_partial_path
+
+      expect(path).to eq("/fields/color/#{page}")
+    end
+  end
+end
