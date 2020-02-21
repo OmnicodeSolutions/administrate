41cae97526d972932297a97494b4260ff8ac2a59
Elvis Serrão <elvis.serrao1@gmail.com>
2020-02-21 08:54:29 -0300

    Remove useless comments

--- app/controllers/administrate/application_controller.rb
+++ app/controllers/administrate/application_controller.rb
@@ -165,7 +165,6 @@ module Administrate
         "administrate.controller.#{key}",
         resource: resource_resolver.resource_title,
       )
-      # t("activerecord.models.#{resource_resolver.resource_title.downcase}.one", resource_resolver.resource_title
     end
 
     def show_search_bar?
