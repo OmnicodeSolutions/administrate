2729bfbefa9992d3890577d9b812d850d4944b02
LucasAzoli <lucasazevedooliveira@outlook.com.br>
2025-07-10 15:29:24 -0300

    chore: add jquery-rails gem and implement jQuery for enhanced page interactions in administrate

--- app/assets/javascripts/administrate/application.js
+++ app/assets/javascripts/administrate/application.js
@@ -1,12 +1,56 @@
 import "@hotwired/turbo-rails"
 import { Application } from "@hotwired/stimulus"
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+import $ from "jquery"
+
+window.$ = window.jQuery = $
+
+import "./components/associative"
+import "./components/date_time_picker" 
+import "./components/select"
+import "./components/table"
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
 
-document.addEventListener("turbo:load", () => {
-})
+function setupPageInteractions() {
+  const deleteLinks = document.querySelectorAll('[data-method="delete"][data-confirm]')
+  deleteLinks.forEach(link => {
+    if (!link.hasAttribute('data-turbo-method')) {
+      link.setAttribute('data-turbo-method', 'delete')
+    }
+    
+    const confirmText = link.getAttribute('data-confirm')
+    if (confirmText && !link.hasAttribute('data-turbo-confirm')) {
+      link.setAttribute('data-turbo-confirm', confirmText)
+    }
+  })
+  
+  const searchForms = document.querySelectorAll('.search')
+  searchForms.forEach(form => {
+    if (!form.submit) {
+      form.submit = function() { 
+        this.dispatchEvent(new Event('submit', { bubbles: true, cancelable: true }))
+      }
+    }
+    
+    if (window.$ && window.$.fn) {
+      $(form).data('legacy-submit', function() {
+        form.submit()
+      })
+    }
+  })
+  
+  window.submitSearchForm = function() {
+    const searchForm = document.querySelector('.search')
+    if (searchForm) {
+      searchForm.submit()
+    }
+  }
+}
+
+document.addEventListener("turbo:load", setupPageInteractions)
+document.addEventListener("DOMContentLoaded", setupPageInteractions)
 
 window.Stimulus = application
