c28f421c27b4f4209205f5ef2803467c263bee55
LucasAzoli <lucasazevedooliveira@outlook.com.br>
2025-09-18 15:38:55 -0300

    refactor: enhance form handling and improve JavaScript asset generator
    - Added error handling for associated resource options in the form to prevent failures during rendering.
    - Updated the JavaScript asset generator to dynamically locate the javascripts directory, improving flexibility and providing a warning if the path is not found.

--- lib/generators/administrate/assets/javascripts_generator.rb
+++ lib/generators/administrate/assets/javascripts_generator.rb
@@ -6,10 +6,25 @@ module Administrate
       class JavascriptsGenerator < Rails::Generators::Base
         JAVASCRIPTS_PATH = "app/assets/javascripts/administrate"
 
-        source_root File.expand_path("../../../../../", __FILE__)
+        source_root File.expand_path("../../../../../../", __FILE__)
 
         def copy_javascripts
-          directory JAVASCRIPTS_PATH, JAVASCRIPTS_PATH
+          possible_paths = [
+            File.expand_path("../../../../../", __FILE__),
+            File.expand_path("../../../../../../", __FILE__),
+            File.expand_path("../../../../../../../", __FILE__)
+          ]
+          
+          source_path = possible_paths.find do |path|
+            File.exist?(File.join(path, JAVASCRIPTS_PATH))
+          end
+          
+          if source_path
+            self.class.source_root source_path
+            directory JAVASCRIPTS_PATH, JAVASCRIPTS_PATH
+          else
+            say "Warning: Could not find #{JAVASCRIPTS_PATH} in any of the expected locations", :yellow
+          end
         end
       end
     end
